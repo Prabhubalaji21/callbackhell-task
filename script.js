9bb5e95e5cd4c075f6159b7b62f3d128f2a7e523
Prabhu balaji <107781010+Prabhubalaji21@users.noreply.github.com>
2023-04-05 19:14:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,36 @@
-const log = (params)=> console.log(params);
-function ten(callback){
-    log("10")
-    setTimeout(callback,1000);
-   
-}
-
-function nine(callback){
-    log("9")
-    setTimeout(callback,1000);
-}
-function eight(callback){
-    log("8")
-    setTimeout(callback,1000)
-}
-
-function seven(callback){
-    log("7")
-    setTimeout(callback,1000)
-}
-
-function six(callback){
-    log("6")
-    setTimeout(callback,1000)
-}
-function five(callback){
-    log("5")
-    setTimeout(callback,1000)
-}
-function four(callback){
-    log("4")
-    setTimeout(callback,1000)
-}
-function three(callback){
-    log("3")
-    setTimeout(callback,1000)
-}function two(callback){
-    log("2")
-    setTimeout(callback,1000)
-}
-function one(callback){
-    log("1")
-    setTimeout(callback,1000)
-}
-function happyIndependenceday(callback){
-    log("Happy Independence Day...!!!")
-    setTimeout(callback,1000)
-}
-
- function task(){
-
-//call back hell 
-    ten(()=>{
-        nine(()=>{
-            eight(()=>{
-                seven(()=>{
-                    six(()=>{
-                        five(()=>{
-                            four(()=>{
-                                three(()=>{
-                                    two(()=>{
-                                        one(()=>{
-                                            happyIndependenceday();
-                                        })
-                                    })
-                                })
-                            })
-                        })
-                    });
-
-                })
-
-            })
-
-        })
-    });
-    
-
-    
-}
-
-task();
-
+let timer = document.createElement("div")
+timer.style.textAlign ="center"
+document.body.append(timer)
+setTimeout(()=>{
+    timer.textContent="10"
+    setTimeout(()=>{
+        timer.textContent="9"
+        setTimeout(()=>{
+            timer.textContent="8"
+            setTimeout(()=>{
+            timer.textContent="7"
+            setTimeout(()=>{
+            timer.textContent="6"
+            setTimeout(()=>{
+            timer.textContent="5"
+            setTimeout(()=>{
+            timer.textContent="4"
+            setTimeout(()=>{
+            timer.textContent="3"
+            setTimeout(()=>{
+            timer.textContent="2"
+            setTimeout(()=>{
+            timer.textContent="1"
+            setTimeout(()=>{
+            timer.textContent="Happy independence Day...!!!!"
+            },1000);
+            },1000);
+            },1000);
+            },1000);
+            },1000);
+            },1000);
+            },1000);
+            },1000);
+            },1000);
+    })
+})
